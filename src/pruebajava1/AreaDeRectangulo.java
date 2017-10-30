e36b91d66f8dde5257fc992c963852018a864f2c
negrita79 <elisa.79@live.cl>
2017-10-30 19:52:30 -0300

    Esta clase calcula el volumen de una esfera,autor Elda Solar, 30/10/2017, clase AreaDeRectangulo y, esta clase calcula el area de un rectangulo segun su base y altura.

---
+++ src/pruebajava1/AreaDeRectangulo.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebajava1;
+
+/**
+ *
+ * @author LAB05
+ */
+public class AreaDeRectangulo 
+{
+    public static void main(String[] args) 
+    {
+      // TODO code application logic here
+  int area_rectangulo = 0;
+       int base = 5;
+       int altura = 4;
+      area_rectangulo = base*altura;
+      System.out.println("El area de un rectangulo de base "+base+" y altura "+altura+" es igual a "+area_rectangulo);  
+}
+    
+        
+ }
